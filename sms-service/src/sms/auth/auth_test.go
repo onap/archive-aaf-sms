1405484bb4752619fea5b79556797fa2814f9cdb
Havaldar Girish <hg0071052@techmahindra.com>
2018-02-12 09:42:47 +0530

    Adding unit test case for auth.go
    Adding Unit Test case auth_test.go and auth_test.cert file for testing auth.go
    
    Change-Id: I68611f2b17a9f37618cbc5ef8db11e6fb632b910
    Issue-ID: AAF-122
    Signed-off-by: Havaldar Girish <hg0071052@techmahindra.com>

---
+++ sms-service/src/sms/auth/auth_test.go
@@ -0,0 +1,42 @@
+/*
+* Copyright 2018 TechMahindra
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+ */
+
+package auth
+
+import (
+	"crypto/tls"
+	"testing"
+)
+
+//Unit test to varify GetTLSconfig func and varify the tls config min version to be 771
+//Assuming cert file name as auth_test.cert
+func TestGetTLSConfig(t *testing.T) {
+	_, err := GetTLSConfig("filedoesnotexist.cert")
+	if err == nil {
+		t.Errorf("Test failed due to expected behaviour")
+	}
+	tlsConfig, err := GetTLSConfig("auth_test.cert")
+	expected := tls.VersionTLS12
+	actual := tlsConfig.MinVersion
+	if tlsConfig != nil {
+		if int(actual) != expected {
+			t.Errorf("Test Failed due to version mismatch")
+		}
+	if tlsConfig == nil {
+		t.Errorf("Test Failed due to GetTLSConfig returned nil")
+	        }
+	}
+}
