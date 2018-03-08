8c18c6463962cffd5eec869f7fa2aff6a6fdbec6
Kiran Kamineni <kiran.k.kamineni@intel.com>
2018-03-08 14:11:15 -0800

    Adding url configuration for endpoint tests
    URL is now a command line parameter for endpoint tests
    
    Issue-ID: AAF-161
    Change-Id: I6f740ad5cbbd3c73ee7e9a4556acef360a1aefa5
    Signed-off-by: Kiran Kamineni <kiran.k.kamineni@intel.com>

--- sms-service/src/sms/test/loop_test.sh
+++ sms-service/src/sms/test/loop_test.sh
@@ -1,57 +1,58 @@
 #!/bin/bash
 RED='\033[0;31m'
 NC='\033[0m'
+URL=$1
 for i in `seq 1 2`; 
 do
   echo -e "${RED}----------------BEGIN GET STATUS----------------${NC}"
   curl -i -w "\n" -H "Accept: application/json" --cacert auth/selfsignedca.pem --cert auth/client.cert --key auth/client.key -X GET \
-    https://localhost:10443/v1/sms/status
+    https://${URL}:10443/v1/sms/status
 
   echo -e "${RED}----------------BEGIN CREATE SECRET DOMAIN------${NC}"
   curl -i -w "\n" -H "Accept: application/json" --cacert auth/selfsignedca.pem --cert auth/client.cert --key auth/client.key -X POST \
-    -d @test/test_create_domain.json https://localhost:10443/v1/sms/domain
+    -d @test/test_create_domain.json https://${URL}:10443/v1/sms/domain
 
   echo -e "${RED}----------------BEGIN CREATE SECRET 1-----------${NC}"
   curl -i -w "\n" -H "Accept: application/json" --cacert auth/selfsignedca.pem --cert auth/client.cert --key auth/client.key -X POST \
-    -d @test/test_create_secret1.json https://localhost:10443/v1/sms/domain/curltestdomain/secret
+    -d @test/test_create_secret1.json https://${URL}:10443/v1/sms/domain/curltestdomain/secret
 
   echo -e "${RED}----------------BEGIN CREATE SECRET 2-----------${NC}"
   curl -i -w "\n" -H "Accept: application/json" --cacert auth/selfsignedca.pem --cert auth/client.cert --key auth/client.key -X POST \
-    -d @test/test_create_secret2.json https://localhost:10443/v1/sms/domain/curltestdomain/secret
+    -d @test/test_create_secret2.json https://${URL}:10443/v1/sms/domain/curltestdomain/secret
 
   echo -e "${RED}----------------BEGIN CREATE SECRET 3-----------${NC}"
   curl -i -w "\n" -H "Accept: application/json" --cacert auth/selfsignedca.pem --cert auth/client.cert --key auth/client.key -X POST \
-    -d @test/test_create_secret3.json https://localhost:10443/v1/sms/domain/curltestdomain/secret
+    -d @test/test_create_secret3.json https://${URL}:10443/v1/sms/domain/curltestdomain/secret
 
   echo -e "${RED}----------------BEGIN LIST SECRET---------------${NC}"
   curl -i -w "\n" -H "Accept: application/json" --cacert auth/selfsignedca.pem --cert auth/client.cert --key auth/client.key -X GET \
-    https://localhost:10443/v1/sms/domain/curltestdomain/secret
+    https://${URL}:10443/v1/sms/domain/curltestdomain/secret
 
   echo -e "${RED}----------------BEGIN GET SECRET 1--------------${NC}"
   curl -i -w "\n" -H "Accept: application/json" --cacert auth/selfsignedca.pem --cert auth/client.cert --key auth/client.key -X GET \
-    https://localhost:10443/v1/sms/domain/curltestdomain/secret/curltestsecret1
+    https://${URL}:10443/v1/sms/domain/curltestdomain/secret/curltestsecret1
 
   echo -e "${RED}----------------BEGIN GET SECRET 2--------------${NC}"
   curl -i -w "\n" -H "Accept: application/json" --cacert auth/selfsignedca.pem --cert auth/client.cert --key auth/client.key -X GET \
-    https://localhost:10443/v1/sms/domain/curltestdomain/secret/curltestsecret2
+    https://${URL}:10443/v1/sms/domain/curltestdomain/secret/curltestsecret2
 
   echo -e "${RED}----------------BEGIN GET SECRET 3--------------${NC}"
   curl -i -w "\n" -H "Accept: application/json" --cacert auth/selfsignedca.pem --cert auth/client.cert --key auth/client.key -X GET \
-    https://localhost:10443/v1/sms/domain/curltestdomain/secret/curltestsecret3
+    https://${URL}:10443/v1/sms/domain/curltestdomain/secret/curltestsecret3
 
   echo -e "${RED}----------------BEGIN DELETE SECRET 1-----------${NC}"
   curl -i -w "\n" -H "Accept: application/json" --cacert auth/selfsignedca.pem --cert auth/client.cert --key auth/client.key -X DELETE \
-    https://localhost:10443/v1/sms/domain/curltestdomain/secret/curltestsecret1
+    https://${URL}:10443/v1/sms/domain/curltestdomain/secret/curltestsecret1
 
   echo -e "${RED}----------------BEGIN DELETE SECRET 2-----------${NC}"
   curl -i -w "\n" -H "Accept: application/json" --cacert auth/selfsignedca.pem --cert auth/client.cert --key auth/client.key -X DELETE \
-    https://localhost:10443/v1/sms/domain/curltestdomain/secret/curltestsecret2
+    https://${URL}:10443/v1/sms/domain/curltestdomain/secret/curltestsecret2
 
   echo -e "${RED}----------------BEGIN DELETE SECRET 3-----------${NC}"
   curl -i -w "\n" -H "Accept: application/json" --cacert auth/selfsignedca.pem --cert auth/client.cert --key auth/client.key -X DELETE \
-    https://localhost:10443/v1/sms/domain/curltestdomain/secret/curltestsecret3
+    https://${URL}:10443/v1/sms/domain/curltestdomain/secret/curltestsecret3
 
   echo -e "${RED}----------------BEGIN DELETE SECRET DOMAIN------${NC}"
   curl -i -w "\n" -H "Accept: application/json" --cacert auth/selfsignedca.pem --cert auth/client.cert --key auth/client.key -X DELETE \
-    https://localhost:10443/v1/sms/domain/curltestdomain
+    https://${URL}:10443/v1/sms/domain/curltestdomain
 done
