ee6d3d18f0678bc1070a7b797952546de34731e2
Manjunath Ranganathaiah <manjunath.ranganathaiah@intel.com>
2018-03-21 09:12:04 -0700

    Test framework for sms client code.
    Adds a test case for create domain interface
    
    Change-Id: I198d4e91845753ca359330c367180bd3a3732f30
    Issue-ID: AAF-183
    Signed-off-by: Manjunath Ranganathaiah <manjunath.ranganathaiah@intel.com>

---
+++ sms-client/src/java/test/SmsCreateDomainTest.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2018 Intel Corporation, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import junit.framework.*;
+import org.onap.aaf.sms.SmsClient;
+import org.onap.aaf.sms.SmsResponse;
+import org.onap.aaf.sms.test.SmsSecureSocket;
+import javax.net.ssl.SSLSocketFactory;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SmsCreateDomainTest extends TestCase {
+
+    public void testSmsCreateDomain() {
+        try {
+            SmsSecureSocket sss = new SmsSecureSocket();
+
+            SmsClient sms = new SmsClient("otconap4.sc.intel.com", 10443, sss.getSSF());
+            SmsResponse resp = sms.createDomain("onap.new.test.sms0");
+            assertTrue(resp.getSuccess());
+            if ( resp.getSuccess() ) {
+                Map<String, Object> m = resp.getResponse();
+                assertNotNull(m);
+                assertEquals(200, resp.getResponseCode());
+                assertEquals("onap.new.test.sms0", m.get("name"));
+            }
+        } catch ( Exception e ) {
+            fail("Exception while creating domain");
+        }
+    }
+}
